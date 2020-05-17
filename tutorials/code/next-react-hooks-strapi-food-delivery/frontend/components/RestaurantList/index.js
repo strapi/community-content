46705ee36630155dbcae73d03e613f652bc70318
ryan <ryanj.belke@gmail.com>
2020-05-16 19:57:20 -0500

    step 2 complete

---
+++ tutorials/code/next-react-hooks-strapi-food-delivery/frontend/components/RestaurantList/index.js
@@ -0,0 +1,93 @@
+/* components/RestaurantList/index.js */
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+import Link from "next/link";
+
+import {
+  Card,
+  CardBody,
+  CardImg,
+  CardText,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
+
+const QUERY = gql`
+  {
+    restaurants {
+      id
+      name
+      description
+      image {
+        url
+      }
+    }
+  }
+`;
+
+function RestaurantList(props) {
+  const { loading, error, data } = useQuery(QUERY);
+  if (error) return "Error loading restaurants";
+  //if restaurants are returned from the GraphQL query, run the filter query
+  //and set equal to variable restaurantSearch
+  if (loading) return <h1>Fetching</h1>;
+  if (data.restaurants && data.restaurants.length) {
+    //searchQuery
+    const searchQuery = data.restaurants.filter((query) =>
+      query.name.toLowerCase().includes(props.search)
+    );
+    if (searchQuery.length != 0) {
+      return (
+        <Row>
+          {searchQuery.map((res) => (
+            <Col xs="6" sm="4" key={res.id}>
+              <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+                <CardImg
+                  top={true}
+                  style={{ height: 250 }}
+                  src={`${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`}
+                />
+                <CardBody>
+                  <CardTitle>{res.name}</CardTitle>
+                  <CardText>{res.description}</CardText>
+                </CardBody>
+                <div className="card-footer">
+                  <Link
+                    as={`/restaurants/${res.id}`}
+                    href={`/restaurants?id=${res.id}`}
+                  >
+                    <a className="btn btn-primary">View</a>
+                  </Link>
+                </div>
+              </Card>
+            </Col>
+          ))}
+
+          <style jsx global>
+            {`
+              a {
+                color: white;
+              }
+              a:link {
+                text-decoration: none;
+                color: white;
+              }
+              a:hover {
+                color: white;
+              }
+              .card-columns {
+                column-count: 3;
+              }
+            `}
+          </style>
+        </Row>
+      );
+    } else {
+      return <h1>No Restaurants Found</h1>;
+    }
+  }
+  return <h5>Add Restaurants</h5>;
+}
+export default RestaurantList;
--- tutorials/code/next-react-hooks-strapi-food-delivery/frontend/pages/_app.js
+++ tutorials/code/next-react-hooks-strapi-food-delivery/frontend/pages/_app.js
@@ -3,7 +3,8 @@ import React from "react";
 import App from "next/app";
 import Head from "next/head";
 import Layout from "../components/Layout";
-export default class MyApp extends App {
+import withData from "../lib/apollo";
+class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
@@ -23,3 +24,4 @@ export default class MyApp extends App {
     );
   }
 }
+export default withData(MyApp);
